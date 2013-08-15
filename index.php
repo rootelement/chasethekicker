83bb803f4f255e81e02eabe2a195ab2611a6baa9
Josh Briley <josh@thebrileys.com>
2013-08-15 16:17:36 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Chase the Kicker</title>
+  <link href='http://fonts.googleapis.com/css?family=Exo:400,800' rel='stylesheet' type='text/css'>
+  <style>
+    html{background-color: #B94102; font-family: 'Exo', sans-serif; color: #fff; text-align: center;}
+    * {padding: 0;margin: 0;}
+    .center {
+       width: 900px;
+       height: 200px;
+       position: absolute;
+       left: 50%;
+       top: 50%; 
+       margin-left: -450px;
+       margin-top: -100px;
+    }
+    h1{font-weight: normal;font-size: 60px;}
+  </style>
+</head>
+<body>
+  <div class="center">
+    <h1>&#10026; Chase The Kicker &#10026;</h1>
+    <p>Coming soon</p>
+  </div>
+  
+</body>
+</html>
