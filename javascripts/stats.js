8502e31da214fcf9dedcd9f3c4415db86b5728a3
Josh Briley <josh@thebrileys.com>
2013-09-06 10:02:34 -0400

    Update stats data.

---
+++ javascripts/stats.js
@@ -0,0 +1,50 @@
+var stats = {
+  week1: {
+    kickoffs: {
+      k1: {
+        distance: 60
+      },
+      k2: {
+        distance: 65
+      },
+      k3: {
+        distance: 63
+      }
+    },
+    punts: {
+      p1: {
+        distance: 35
+      },
+      p2: {
+        distance: 44
+      },
+      p3: {
+        distance: 40
+      }
+    },
+    fieldGoals: {
+      fg1: {
+        distance: 39,
+        result: 1
+      },
+      fg2: {
+        distance: 28,
+        result: 0
+      }
+    },
+    pat: {
+      pat1: {
+        result: 1
+      },
+      pat2: {
+        result: 1
+      },
+      pat3: {
+        result: 1
+      },
+      pat4: {
+        result: 1
+      },
+    }
+  }
+}
